1126e60c1774d728e3e7083dadc64b22f8d6c67a
Daniel <danielmahal@gmail.com>
2013-11-20 22:18:12 +0100

    Sending values serially

--- core/core.ino
+++ core/core.ino
@@ -7,10 +7,13 @@ void setup() {
 
 void loop() {
   int pulseTimeout = 256 * 60 * 2;
+  
   int raw = floor(float(pulseIn(in, HIGH, pulseTimeout) / 256.0 / 60) * 255);
+
   int i = floor(raw / 50);
   int value = (float(raw - (i * 50)) / 50.0) * 255.0;
-
+  
+//  Serial.println(raw);
   Serial.print(i);
   Serial.print(": ");
   Serial.print(value);
--- joint/joint.ino
+++ joint/joint.ino
@@ -1,19 +1,64 @@
+int in = 3;
+int pot = 2;
 int out = 0;
+boolean inConnected = false;
 
-int values[] = { 22, 54, 83, 130 };
-int i = 0;
+int values[] = { 100, 0 };
+int idOut = 0;
+int connectionCounter = 0;
+int pulseTimeout = 256 * 60 * 2;
 
 void setup() {
+  pinMode(in, INPUT);
   pinMode(out, OUTPUT);
 }
 
 void loop() {
+  if(inConnected) {
+    connectionCounter = 0;
+    
+    int raw = getPulseIn();
+    
+    if(raw == 0) {
+      inConnected = false;
+    } else {
+      values[1] = raw;
+    }
+  } else {
+    connectionCounter++;
+    
+    if(connectionCounter == 100) {
+      connectionCounter = 0;
+      int check = pulseIn(in, HIGH, pulseTimeout);
+      if(check > 0)
+        inConnected = true;
+    }
+  }
+  
+//  int compound = ;
+//  int idIn = floor(compound / 50);
+//  int value = (float(compound - (idIn * 50)) / 50.0) * 255.0;
+//  values[1] = getPulseIn();
+  
+//  values[0] = (float(analogRead(pot)) / 1024.0) * 255;
+  
   // Out
-  i++;
-  if(i > 3) i = 0;
+  idOut++;
+  if(idOut >= 2) idOut = 0;
   
-  int value = (float(values[i]) / 255.0) * 50;
-  int sendValue = ((i) * 50) + value;
+  int valueOut = (float(values[idOut]) / 255.0) * 50;
+  valueOut += (idOut) * 50;
   
-  analogWrite(out, sendValue);
+  analogWrite(out, valueOut);
+//  analogWrite(out, values[0]);
+
+  
+
+//  int valueOut = (float(getPulseIn()) / 255.0) * 50;
+//  valueOut += 0 * 50;
+//  analogWrite(out, valueOut);
+}
+
+int getPulseIn() {
+  return pulseIn(in, HIGH, pulseTimeout) / 1.05;
 }
